70281b9b4c3b95f48bef8af0ddef09999d6dd3f2
wdhg <wdhgrant@gmail.com>
2020-10-06 12:52:55 +0100

    add basic data types to represent formulae

--- src/Pithos/Base.hs
+++ src/Pithos/Base.hs
@@ -1,1 +1,33 @@
 module Pithos.Base where
+
+newtype Var
+  = Var String
+
+newtype Const
+  = Const String
+
+newtype Arity
+  = Arity Int
+
+data RelationSymbol
+  = RelationSymbol String Arity
+
+data Term
+  = TVar Var
+  | TConst Const
+
+data Atom
+  = Relation RelationSymbol [Term]
+  | Equal Term Term
+  | Truth
+  | Falsity
+
+data Formula
+  = Atomic Atom
+  | Not Formula
+  | And Formula Formula
+  | Or Formula Formula
+  | If Formula Formula
+  | Iff Formula Formula
+  | ForAll Var Formula
+  | Exists Var Formula
